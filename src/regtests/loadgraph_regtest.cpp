35f2333be6b15ee1ae39bfda4dbd43e5fc22769e
Ferran Olivera <ferranolivera@gmail.com>
2018-06-13 19:59:23 +0200

    New regtest to check correctness of persisted graphs.

--- src/regtests/loadgraph_regtest.cpp
+++ src/regtests/loadgraph_regtest.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <memory/buffer_pool.h>
+#include <tasking/tasking.h>
 
 SMILE_NS_BEGIN
 
@@ -103,6 +104,112 @@ TEST(PerformanceTest, PerformanceTestLoadGraph) {
 
 		delete firstNbr;
 		delete Nbr;
+	}
+}
+
+/**
+ * Checks that a graph has correctly been persisted into a DB. The program loads the graph from a DB
+ * and tests its correctness by comparing it to the one defined in PATH_TO_GRAPH_FILE.
+ * 
+ * Graph files must have the following structure:
+ * 
+ * 		number_of_nodes		number_of_edges
+ * 		node1				neighbor1_of_node1
+ * 		node1				neighbor2_of_node1
+ * 		node2				neighbor1_of_node2
+ * 		node3				neighbor1_of_node3
+ * 		node3				neighbor2_of_node3
+ * 		...
+ * 
+ * Graph should be stored in the DB with the following structure:
+ * 
+ * 		Page:		Data:
+ * 		0			Metadata --> number of nodes, number of edges, firstNbr starting page (fnp), Nbr starting page (np)
+ * 		fnp - np	firstNbr structure
+ * 		np - ...	Nbr structure
+ * 		
+ */
+TEST(PerformanceTest, PerformanceTestCheckGraph) {
+	if (PATH_TO_GRAPH_FILE != "" || !std::ifstream("./graph.db")) {
+		uint32_t* fileFirstNbr;
+		uint32_t* fileNbr;
+		uint32_t fileNumNodes = 0, fileNumEdges = 0;
+
+		// Load graph from file
+		std::ifstream graphFile;
+	  	graphFile.open(PATH_TO_GRAPH_FILE);
+	  	if (graphFile.is_open()) {
+		  	uint32_t  orig, dest, lastOrig;
+		  	graphFile >> fileNumNodes;
+		  	graphFile >> fileNumEdges;
+		  	fileFirstNbr = new uint32_t[fileNumNodes]();
+		  	fileNbr = new uint32_t[fileNumEdges]();
+
+			for (uint32_t i = 0; i < fileNumEdges; ++i) {
+				graphFile >> orig;
+	  			graphFile >> dest;
+
+	  			if (orig != lastOrig || i == 0) {
+	  				fileFirstNbr[orig] = i;
+	  			}
+	  			lastOrig = orig;
+
+	  			fileNbr[i] = dest;
+			}
+		}
+	  	graphFile.close();
+
+	  	// Open DB containing the graph
+	  	startThreadPool(1);
+	  	BufferPool bufferPool;
+		ASSERT_TRUE(bufferPool.open(BufferPoolConfig{1024*1024}, "./graph.db") == ErrorCode::E_NO_ERROR);
+		BufferHandler bufferHandler;
+
+		// Check DB-graph metadata
+		ASSERT_TRUE(bufferPool.pin(1, &bufferHandler) == ErrorCode::E_NO_ERROR);
+		uint32_t* buffer = reinterpret_cast<uint32_t*>(bufferHandler.m_buffer);
+		ASSERT_TRUE(buffer[0] == fileNumNodes);
+		ASSERT_TRUE(buffer[1] == fileNumEdges);
+		uint32_t firstNbrPage = buffer[2];
+		uint32_t NbrPage = buffer[3];
+
+		// Load DB-graph data
+		uint32_t* dbFirstNbr = new uint32_t[fileNumNodes]();
+		uint32_t* dbNbr = new uint32_t[fileNumEdges]();
+
+		uint32_t elemsPerPage = (PAGE_SIZE_KB*1024)/sizeof(uint32_t);
+		int remainingBytes = fileNumNodes*sizeof(uint32_t);
+		for (uint32_t i = 0; i < fileNumNodes; i += elemsPerPage) {
+			ASSERT_TRUE(bufferPool.pin(firstNbrPage+(i/elemsPerPage), &bufferHandler) == ErrorCode::E_NO_ERROR);
+			memcpy(&dbFirstNbr[i], bufferHandler.m_buffer, std::min(PAGE_SIZE_KB*1024, remainingBytes));
+			remainingBytes -= PAGE_SIZE_KB*1024;
+			ASSERT_TRUE(bufferPool.unpin(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR);
+		}
+
+		remainingBytes = fileNumEdges*sizeof(uint32_t);
+		for (uint32_t i = 0; i < fileNumEdges; i += elemsPerPage) {
+			ASSERT_TRUE(bufferPool.pin(NbrPage+(i/elemsPerPage), &bufferHandler) == ErrorCode::E_NO_ERROR);
+			memcpy(&dbNbr[i], bufferHandler.m_buffer, std::min(PAGE_SIZE_KB*1024, remainingBytes));
+			remainingBytes -= PAGE_SIZE_KB*1024;
+			ASSERT_TRUE(bufferPool.unpin(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR);
+		}
+
+		// Check firstNbr and Nbr
+		for (uint32_t i = 0; i < fileNumNodes; ++i) {
+			ASSERT_TRUE(fileFirstNbr[i] == dbFirstNbr[i]);
+		}
+
+		for (uint32_t i = 0; i < fileNumEdges; ++i) {
+			ASSERT_TRUE(fileNbr[i] == dbNbr[i]);
+		}
+
+		stopThreadPool();
+		ASSERT_TRUE(bufferPool.close() == ErrorCode::E_NO_ERROR);
+
+		delete fileFirstNbr;
+		delete fileNbr;
+		delete dbFirstNbr;
+		delete dbNbr;
 	}
 }
 

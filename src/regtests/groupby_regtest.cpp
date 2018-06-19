34400b913485fd834c2419dc79db2bd8ac98a260
Ferran Olivera <ferranolivera@gmail.com>
2018-06-19 19:16:05 +0200

    Use of OpenMP to speedup GroupBy execution.

--- src/regtests/groupby_regtest.cpp
+++ src/regtests/groupby_regtest.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 #include <memory/buffer_pool.h>
 #include <tasking/tasking.h>
+#include <omp.h>
 
 SMILE_NS_BEGIN
 
 #define PAGE_SIZE_KB 64
 #define DATA_KB 4*1024*1024
+#define NUM_THREADS 4
 
 /**
  * Tests a Group By operation of 4GB over a 1GB-size Buffer Pool for benchmarking purposes.
@@ -18,33 +20,37 @@ TEST(PerformanceTest, PerformanceTestGroupBy) {
 		ASSERT_TRUE(bufferPool.open(BufferPoolConfig{1024*1024}, "./test.db") == ErrorCode::E_NO_ERROR);
 		BufferHandler bufferHandler;
 
-		uint64_t page = 0;
-
 		// GroupBy operation
-		for (uint64_t i = 0; i < DATA_KB; i += PAGE_SIZE_KB) {
-			if ( page%(PAGE_SIZE_KB*1024*8) == 0 ) ++page;
-			ASSERT_TRUE(bufferPool.pin(page, &bufferHandler) == ErrorCode::E_NO_ERROR);
-			
-			std::map<uint8_t, uint16_t> occurrencesMap;
-			uint8_t* buffer = reinterpret_cast<uint8_t*>(bufferHandler.m_buffer);
-			for (uint64_t byte = 0; byte < PAGE_SIZE_KB*1024; ++byte) {
-				uint8_t number = *buffer;
-				std::map<uint8_t, uint16_t>::iterator it;
-				it = occurrencesMap.find(number);
+		#pragma omp parallel num_threads(NUM_THREADS)
+		{
+			uint64_t threadID = omp_get_thread_num();
+			uint64_t numThreads = omp_get_num_threads();
+			uint64_t KBPerThread = DATA_KB/numThreads;
 
-				if (it == occurrencesMap.end()) {
-					occurrencesMap.insert(std::pair<uint8_t,uint16_t>(number,1));
-				}
-				else {
-					++it->second;
+			for (uint64_t i = threadID*KBPerThread; (i-threadID*KBPerThread) < KBPerThread; i += PAGE_SIZE_KB) {
+				uint64_t page = 1 + (i/PAGE_SIZE_KB)/(PAGE_SIZE_KB*1024) + (i/PAGE_SIZE_KB);
+				bufferPool.pin(page, &bufferHandler);
+				
+				std::map<uint8_t, uint16_t> occurrencesMap;
+				uint8_t* buffer = reinterpret_cast<uint8_t*>(bufferHandler.m_buffer);
+				for (uint64_t byte = 0; byte < PAGE_SIZE_KB*1024; ++byte) {
+					uint8_t number = *buffer;
+					std::map<uint8_t, uint16_t>::iterator it;
+					it = occurrencesMap.find(number);
+
+					if (it == occurrencesMap.end()) {
+						occurrencesMap.insert(std::pair<uint8_t,uint16_t>(number,1));
+					}
+					else {
+						++it->second;
+					}
+
+					++buffer;
 				}
 
-				++buffer;
+				bufferPool.unpin(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR;
 			}
-
-			ASSERT_TRUE(bufferPool.unpin(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR);
-			++page;
-		}
+		}		
 
 		stopThreadPool();
 	}
